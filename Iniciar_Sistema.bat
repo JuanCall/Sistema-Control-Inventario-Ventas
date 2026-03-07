b4fcc7d2ff74555ad8256b20ee7a71b83344219c
JuanCall <juancallerosales19@gmail.com>
2026-03-07 17:46:41 -0500

    Update V2 - Inventario y venta por Lotes

--- Iniciar_Sistema.bat
+++ Iniciar_Sistema.bat
@@ -1,5 +1,14 @@
 @echo off
-echo Iniciando SistemaSmart... Por favor no cierre esta ventana negra.
+title Motor del SistemaSmart - NO CERRAR
+echo ======================================================
+echo          SISTEMA DE INVENTARIO ACTIVO
+echo ======================================================
+echo.
+echo Por favor, NO cierre esta ventana mientras la farmacia este abierta.
+echo Puede minimizarla (-) para que no estorbe en la pantalla.
+echo.
+echo Al terminar el turno y cerrar la caja, cierre esta ventana (X).
+echo.
 call venv\Scripts\activate
 start http://127.0.0.1:8000
 uvicorn app.main:app
